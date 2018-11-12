e2c0828b2850c1a2f7e5c6dc0340104dd08f4ec0
Stefan Wawrzyn <swawrzyn@gmail.com>
2018-11-12 16:20:53 +0800

    completed exercises up to scoring, need to now store in session

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,29 @@
+require 'json'
+require 'open-uri'
+require 'time'
+
 class GamesController < ApplicationController
   def new
+    @letters = []
+    @start_time = Time.now
+    10.times { @letters << ('A'..'Z').to_a.sample }
   end
 
   def score
+    @word = params['word']
+    url = open("https://wagon-dictionary.herokuapp.com/#{@word.downcase}").read
+    @letters = params['letters'].split
+    @total_time =(Time.now - Time.parse(params['start_time'])).round(2)
+
+    def canBuild?(letters, guess)
+      letter_hash = Hash.new(0)
+      letters.each { |letter| letter_hash[letter.to_sym] += 1 }
+      guess.upcase.split('').each { |guess_letter| letter_hash[guess_letter.to_sym] -= 1 }
+      letter_hash.none? { |_key, value| value.negative? }
+    end
+
+    @real_word = JSON.parse(url)['found']
+    @can_build = canBuild?(@letters, @word)
+    @score = ((@word.length * 1/@total_time) * 100).floor
   end
 end
